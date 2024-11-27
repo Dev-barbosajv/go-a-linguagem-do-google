3f29b72f7fe526cacea4df82f7a7e81abf98459d
Dev-barbosajv <joaovitorbarbosa513@gmail.com>
2024-11-27 09:00:36 -0300

    chamando pkg Inferencias de tipos

--- cmd/execute.go
+++ cmd/execute.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	hello := pkg.Hello()
 	variables := pkg.Types_variables()
+	inferencias := pkg.Inferencias_de_tipos()
 	fmt.Println(hello)
 	fmt.Println(variables)
+	fmt.Println(inferencias)
 }
