0022425c947753f6b99d1fd92fb8c9abe202866f
Dev-barbosajv <joaovitorbarbosa513@gmail.com>
2024-11-27 21:38:48 -0300

    comentando pacotes

--- cmd/execute.go
+++ cmd/execute.go
@@ -1,16 +1,10 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Dev-barbosajv/go-a-linguagem-do-google/pkg"
-)
-
 func main() {
-	hello := pkg.Hello()
-	variables := pkg.Types_variables()
-	inferencias := pkg.Inferencias_de_tipos()
-	fmt.Println(hello)
-	fmt.Println(variables)
-	fmt.Println(inferencias)
+	//hello := pkg.Hello()
+	//variables := pkg.Types_variables()
+	//inferencias := pkg.Inferencias_de_tipos()
+	//fmt.Println(hello)
+	//fmt.Println(variables)
+	//fmt.Println(inferencias)
 }
