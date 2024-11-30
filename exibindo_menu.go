58709b6e61a491a86f5d1ffbcc326daf0918738b
Dev-barbosajv <joaovitorbarbosa513@gmail.com>
2024-11-29 22:20:55 -0300

    introdução as funçoes

--- exibindo_menu.go
+++ exibindo_menu.go
@@ -1,23 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("1- Iniciar monitoramento.")
 	fmt.Println("2- Exibir logs.")
 	fmt.Println("0-  Sair do programa.")
 
-	var comando int
-	fmt.Scan(&comando)
-	//fmt.Println("O comando escolhido foi:", comando)
-
-	//if comando == 1 {
-	//	fmt.Println("Iniciar monitoramento.")
-	//} else if comando == 2 {
-	//	fmt.Println("Exibir logs.")
-	//} else {
-	//	fmt.Println("Sair do programa.")
-	//}
+	comando := commandLeen()
 
 	switch comando {
 	case 1:
@@ -26,8 +19,18 @@ func main() {
 		fmt.Println("Exibir logs.")
 	case 0:
 		fmt.Println("Sair do programa.")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 
 	}
+}
+
+func commandLeen() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
